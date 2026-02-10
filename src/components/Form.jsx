b2e58cca6fa6a5b3c13234957b2144689834c587
Ruben <rubenrm.contact@gmail.com>
2026-02-10 20:27:29 +0100

    Formulario del primer trimestre con validaciones propias

---
+++ src/components/Form.jsx
@@ -0,0 +1,95 @@
+import useForm from "../hooks/useForm";
+
+export default function Form() {
+  const {
+    formData,
+    formErrores,
+    validarNombre,
+    validarEdad,
+    validarDireccion,
+    validarClase,
+    handleChange,
+    handleClassErrors,
+    handleSubmit,
+  } = useForm();
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-md mx-auto p-6 bg-white rounded-xl shadow-lg"
+      noValidate
+    >
+      {/* Nombre */}
+      <div className="mb-6">
+        <label className="block text-sm font-medium text-gray-700 mb-2">
+          Nombre
+        </label>
+        <input
+          type="text"
+          name="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+          className={handleClassErrors("nombre", validarNombre)}
+        />
+      </div>
+
+      {/* Edad */}
+      <div className="mb-6">
+        <label className="block text-sm font-medium text-gray-700 mb-2">
+          Edad
+        </label>
+        <input
+          type="number"
+          name="edad"
+          value={formData.edad}
+          onChange={handleChange}
+          className={handleClassErrors("edad", validarEdad)}
+        />
+      </div>
+
+      {/* Dirección */}
+      <div className="mb-6">
+        <label className="block text-sm font-medium text-gray-700 mb-2">
+          Dirección
+        </label>
+        <input
+          type="text"
+          name="direccion"
+          value={formData.direccion}
+          onChange={handleChange}
+          className={handleClassErrors("direccion", validarDireccion)}
+        />
+      </div>
+
+      {/* Clase */}
+      <div className="mb-6">
+        <label className="block text-sm font-medium text-gray-700 mb-2">
+          Clase
+        </label>
+        <select
+          name="clase"
+          value={formData.clase}
+          onChange={handleChange}
+          className={handleClassErrors("clase", validarClase)}
+        >
+          <option value="">Selecciona una clase</option>
+          <option value="rosas">Rosas</option>
+          <option value="girasoles">Girasoles</option>
+          <option value="cerezos">Cerezos</option>
+        </select>
+      </div>
+
+      <button
+        type="submit"
+        className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg font-medium hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200 disabled:bg-gray-400 disabled:cursor-not-allowed"
+      >
+        Enviar
+      </button>
+      {formErrores &&
+        //Lo convierte de nuevo a una rray con indice y valor, para poder mapearlo y recorrerlo
+        Object.values(formErrores).map((error, index) => (
+          <p key={index}>{error}</p>
+        ))}
+    </form>
+  );
+}
---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+
+const INIT_FORM = {
+  nombre: "",
+  edad: 0,
+  direccion: "",
+  telefono: "",
+  clase: "",
+  alergenos: [],
+};
+const INIT_FORM_ERRORS = {
+  nombre: "",
+  edad: null,
+  direccion: "",
+  telefono: "",
+  clase: "",
+};
+
+export default function useForm() {
+  const [formData, setFormData] = useState(INIT_FORM);
+  const [formErrores, setFormErrors] = useState(INIT_FORM_ERRORS);
+
+  function validarNombre(texto) {
+    if (!texto.trim()) return "El nombre es obligatorio";
+    if (texto.length < 3 || texto.length > 50)
+      return "El nombre debe contar con entre 3 y 50 caracteres";
+    return null;
+  }
+
+  function validarEdad(edad) {
+    if (!edad) return "La edad es obligatoria";
+    if (edad < 3 || edad > 5) return "La edad debe estar entre 3 y 5 años.";
+    return null;
+  }
+
+  function validarDireccion(direccion) {
+    if (!direccion.trim()) return "La direccion es obligatoria";
+    if (direccion.length > 50)
+      return "La direccion debe contar con un maximo de 50 caracteres.";
+    return null;
+  }
+  function validarClase(clase) {
+    if (!clase) return "Elige alguna de las clases";
+    if (clase !== "rosas" && clase !== "girasoles" && clase !== "cerezos")
+      return "Elige una clase valida";
+    return null;
+  }
+
+  //Maneja los cambios de todos los elementos
+  function handleChange(e) {
+    //El target nos devuelve el name del elemento activador y el value dado
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  //Al enviar creamos array local con los errores que existan
+  function handleSubmit(e) {
+    e.preventDefault();
+    const nuevosErrores = {
+      nombre: validarNombre(formData.nombre),
+      edad: validarEdad(formData.edad),
+      direccion: validarDireccion(formData.direccion),
+      clase: validarClase(formData.clase),
+    };
+    //Convierte el objeto local en un array con solo los valores de los errores, coge solo aquellos distintos a null
+    const erroresFiltrados = Object.values(nuevosErrores).filter(
+      (el) => el && el !== "",
+    );
+    console.log(erroresFiltrados);
+
+    //Si este array de valores es mayor que 0 significa q hay errores, asi que los seteamos
+    if (erroresFiltrados.length > 0) {
+      setFormErrors(erroresFiltrados);
+
+      return;
+    }
+    alert("Formulario enviado");
+  }
+
+  //Tenemos una clase base y en base a un elemento falsy que tenemos al validar el campos que le pasemos, vemos si es invalido(distinto a null) o no,
+  //Si es null retornamos clase base, si no la clase invalida
+  function handleClassErrors(field, validator) {
+    const baseClase =
+      "w-full p-3 border rounded-lg border-gray-300 bg-white focus:border-blue-500 focus:ring-2 focus:ring-blue-100";
+
+    const isInvalid = validator(formData[field]);
+    if (isInvalid)
+      return (
+        baseClase + " border-red-500 bg-red-50 focus:ring-2 focus:ring-red-200"
+      );
+    else {
+      return baseClase;
+    }
+  }
+
+  return {
+    formData,
+    formErrores,
+    validarNombre,
+    validarEdad,
+    validarDireccion,
+    validarClase,
+    handleChange,
+    handleClassErrors,
+    handleSubmit,
+  };
+}
